7aa09ec70416cf5f5550b57925a32d026fd71f60
Christina BarrettiSigal <CBarrettiSigal@gmail.com>
2020-05-04 13:59:23 -0400

    implemented Cockpit, plus a few security updates and more reorganizational edits

---
+++ udemy-react-complete-guide/my-app/src/components/Cockpit/Cockpit.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import classes from './Cockpit.module.css';
+
+const cockpit = (props) => {
+    const assignedClasses = [];
+    let btnClass = '';
+
+    if (props.showPersons) {
+      btnClass = classes.Red;
+    }
+
+    if (props.persons.length <= 2) {
+      assignedClasses.push(classes.red); // classes = ['red']
+    }
+    // this is different from an else if because we want BOTH
+
+    if (props.persons.length <= 1) {
+      assignedClasses.push(classes.bold); // classes = ['red', 'bold']
+    }
+
+  return (
+    <div className={classes.Cockpit}>
+      <h1>Hi, I'm a React App by Cricket</h1>
+      <p className={assignedClasses.join( ' ' )}>It sure is a relief when this works...</p>
+      <button
+        className={btnClass}
+        onClick={props.clicked}>Toggle People</button>
+    </div>
+    );
+}
+
+export default cockpit;
--- udemy-react-complete-guide/my-app/src/containers/App.js
+++ udemy-react-complete-guide/my-app/src/containers/App.js
@@ -1,23 +1,8 @@
 import React, { Component } from 'react';
 // import styled from 'styled-components';
-
+import Cockpit from '../components/Cockpit/Cockpit.js'
 import classes from './App.module.css';
-import Person from '../components/Persons/Person/Person.js';
-
-// const StyledButton = styled.button`
-//   background-color: ${props => props.altStyle ? 'red' : 'green'};
-//   color: white;
-//   font: inherit;
-//   border: 1px solid blue;
-//   padding: 8px;
-//   cursor: pointer;
-//   border-radius: 5px;
-
-//   &:hover {
-//     background-color: ${props => props.altStyle ? 'salmon' : 'lightgreen'};
-//     color: black;
-//   }
-// `;
+import Persons from '../components/Persons/Persons.js';
 
 class App extends Component {
   state = {
@@ -39,8 +24,6 @@ class App extends Component {
       ...this.state.persons[personIndex]
     };
 
-    // const person = Object.assign({}, this.state.persons[personIndex]);
-
     person.name = event.target.value;
 
     const persons = [...this.state.persons];
@@ -55,7 +38,6 @@ class App extends Component {
     persons.splice(personIndex, 1);
     this.setState({ persons: persons });
   };
-
   // one option for immutability: slice method copies the array before altering it
     // const persons = this.state.persons.slice();
     // or, 2nd option; create a new array using spread operator and put contents of old persons array into it
@@ -65,78 +47,25 @@ class App extends Component {
   };
 
   render() {
-    // const buttonStyle = {
-    //   backgroundColor: '#195106',
-    //   color: '#0fc7be',
-    //   font: 'inherit',
-    //   border: '1px solid blue',
-    //   padding: '8px',
-    //   cursor: 'pointer',
-    //   borderRadius: '4px',
-    //   ':hover': {
-    //     backgroundColor: 'lightgreen',
-    //     color: 'black'
-    //   }
-    // };
-
     let persons = null;
-    let btnClass = [classes.Button];
 
     if (this.state.showPersons) {
-      persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return (
-              <Person
-                click={() => this.deletePersonHandler(index)}
-                name={person.name}
-                age={person.age}
-                key={person.id}
-                changed={event => this.nameChangedHandler(event, person.id)}
-              />
-            );
-          })}
-        </div>
-      );
-
-      btnClass.push(classes.Red);
-      // buttonStyle.backgroundColor = 'hsl(312, 81%, 37%)';
-      //   buttonStyle[':hover'] = {
-      //     color: 'brown',
-      //     backgroundColor: 'pink'
-      //   }
-      //   buttonStyle.color = 'hsl(182, 81%, 70%)'
+      persons =
+        <Persons
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangedHandler} />;
     }
 
-    const assignedClasses = [];
-    if (this.state.persons.length <= 2) {
-      assignedClasses.push('red'); // classes = ['red']
-    }
-    // this is different from an else if because we want BOTH
-
-    if (this.state.persons.length <= 1) {
-      assignedClasses.push('bold'); // classes = ['red', 'bold']
-    }
-
-      // we need join because now classes is an array but we need a string
-      // without styled components package, 'StyledButton' goes back to being 'button'
-      // style={buttonStyle} was removed from button for styled-components
     return (
       <div className={classes.App}>
-        <h1>Hi, I'm a React App by Cricket</h1>
-        <p className={assignedClasses.join( ' ' )}>It sure is a relief when this works...</p>
-        <button
-          className={btnClass.join(' ')}
-          onClick={this.togglePersonsHandler}>Toggle People</button>
+      <Cockpit
+        showPersons={this.state.showPersons}
+        persons={this.state.persons}
+        clicked={this.togglePersonsHandler} />
         {persons}
       </div>
     );
-
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
-        // previous StyledButton component, set aside for now
-        // <StyledButton altStyle={this.state.showPersons} onClick={this.togglePersonsHandler}>
-        // Toggle Persons
-        // </StyledButton>
   }
 }
 
